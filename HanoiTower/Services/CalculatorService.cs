52c57c04898161468e80dee9c4ba9f946d2e987a
Németi Dávid <david.nemeti@tel.io>
2022-08-10 15:11:18 +0200

    Hanoi initial parameters can be edited on UI

---
+++ HanoiTower/Services/CalculatorService.cs
@@ -0,0 +1,14 @@
+﻿using HanoiTower.ViewModel;
+
+namespace HanoiTower.Services
+{
+    public class CalculatorService
+    {
+        public event EventHandler<HanoiInitialData>? Calculate;
+
+        public void OnCalculate(object sender, HanoiInitialData data)
+        {
+            Calculate?.Invoke(sender, data);
+        }
+    }
+}
---
+++ HanoiTower/ViewModels/HanoiInitialData.cs
@@ -0,0 +1,11 @@
+﻿using HanoiTower.Core;
+
+namespace HanoiTower.ViewModel
+{
+    public class HanoiInitialData
+    {
+        public int NumberOfDisks { get; set; }
+        public HanoiRod SourceRod { get; set; }
+        public HanoiRod TargetRod { get; set; }
+    }
+}
