dbe545a5fcc4a08a0dcca1a2cd86db40ae8b53bf
Németi Dávid <david.nemeti@tel.io>
2022-08-23 16:06:15 +0200

    HanoiExplanation: Depth

--- HanoiTower.Core/HanoiSolverExplainer.cs
+++ HanoiTower.Core/HanoiSolverExplainer.cs
@@ -8,7 +8,7 @@ namespace HanoiTower.Core
         protected override HanoiTowerExplanationContext<THanoiTower> Solve(HanoiTowerExplanationContext<THanoiTower> tower, HanoiGoal goal)
         {
             var beginState = tower;
-            var endState = base.Solve(tower, goal);
+            var endState = base.Solve(tower.WithIncreasedDepth(), goal).WithDecreasedDepth();
 
             var move = goal.Move.WithIndex(tower.Step + 1);
 
@@ -24,26 +24,34 @@ namespace HanoiTower.Core
         private readonly THanoiTower _tower;
         private readonly ImmutableStack<HanoiExplanation<THanoiTower>> _explanations;
         public int Step { get; }
+        public int Depth { get; }
 
         public HanoiTowerExplanationContext(THanoiTower tower)
-            : this(tower, ImmutableStack.Create<HanoiExplanation<THanoiTower>>(), step: 0)
+            : this(tower, ImmutableStack.Create<HanoiExplanation<THanoiTower>>(), step: 0, depth: 0)
         {
         }
 
-        private HanoiTowerExplanationContext(THanoiTower tower, ImmutableStack<HanoiExplanation<THanoiTower>> explanations, int step)
+        private HanoiTowerExplanationContext(THanoiTower tower, ImmutableStack<HanoiExplanation<THanoiTower>> explanations, int step, int depth)
             : base(tower.NumberOfDisks)
         {
             _tower = tower;
             _explanations = explanations;
             Step = step;
+            Depth = depth;
         }
 
-        public override HanoiTowerExplanationContext<THanoiTower> MoveDisk(HanoiMove move) => new HanoiTowerExplanationContext<THanoiTower>(_tower.MoveDisk(move), _explanations, Step + 1);
+        public override HanoiTowerExplanationContext<THanoiTower> MoveDisk(HanoiMove move) => new HanoiTowerExplanationContext<THanoiTower>(_tower.MoveDisk(move), _explanations, Step + 1, Depth);
+
+        public HanoiTowerExplanationContext<THanoiTower> WithIncreasedDepth() =>
+            new HanoiTowerExplanationContext<THanoiTower>(_tower, _explanations, Step, Depth + 1);
+
+        public HanoiTowerExplanationContext<THanoiTower> WithDecreasedDepth() =>
+            new HanoiTowerExplanationContext<THanoiTower>(_tower, _explanations, Step, Depth - 1);
 
         public HanoiTowerExplanationContext<THanoiTower> WithSimpleExplanation(HanoiTowerExplanationContext<THanoiTower> beginState, HanoiGoal goal, IndexedHanoiMove move)
         {
-            var explanations = _explanations.Push(HanoiExplanation.Simple(beginState._tower, goal, move, endState: _tower, Step));
-            return new HanoiTowerExplanationContext<THanoiTower>(_tower, explanations, Step);
+            var explanations = _explanations.Push(HanoiExplanation.Simple(beginState._tower, goal, move, endState: _tower, Step, Depth));
+            return new HanoiTowerExplanationContext<THanoiTower>(_tower, explanations, Step, Depth);
         }
 
         public HanoiTowerExplanationContext<THanoiTower> WithComplexExplanation(HanoiTowerExplanationContext<THanoiTower> beginState, HanoiGoal goal, IndexedHanoiMove move)
@@ -52,9 +60,9 @@ namespace HanoiTower.Core
                 .Pop(out var explanationAfterMove)
                 .Pop(out var explanationMove)
                 .Pop(out var explanationBeforeMove)
-                .Push(HanoiExplanation.Complex(beginState._tower, explanationBeforeMove, goal, explanationMove.Move, explanationAfterMove, endState: _tower, Step));
+                .Push(HanoiExplanation.Complex(beginState._tower, explanationBeforeMove, goal, explanationMove.Move, explanationAfterMove, endState: _tower, Step, Depth));
 
-            return new HanoiTowerExplanationContext<THanoiTower>(_tower, explanations, Step);
+            return new HanoiTowerExplanationContext<THanoiTower>(_tower, explanations, Step, Depth);
         }
 
         public HanoiExplanation<THanoiTower> GetExplanation() => _explanations.Single();
--- HanoiTower.Core/IHanoiSolution.cs
+++ HanoiTower.Core/IHanoiSolution.cs
@@ -13,26 +13,28 @@
         public THanoiTower BeginState { get; }
         public HanoiGoal Goal { get; }
         public IndexedHanoiMove Move { get; }
+        public int Depth { get; }
         public THanoiTower EndState { get; }
         public HanoiExplanationLocation Location { get; init; }
 
         protected int Step { get; }
 
-        private HanoiExplanation(THanoiTower beginState, HanoiGoal goal, IndexedHanoiMove move, THanoiTower endState, int step)
+        private HanoiExplanation(THanoiTower beginState, HanoiGoal goal, IndexedHanoiMove move, THanoiTower endState, int step, int depth)
         {
             BeginState = beginState;
             Goal = goal;
             Move = move;
             EndState = endState;
             Step = step;
+            Depth = depth;
         }
 
         public sealed record Simple : HanoiExplanation<THanoiTower>
         {
             public new int Step => base.Step;
 
-            public Simple(THanoiTower beginState, HanoiGoal goal, IndexedHanoiMove move, THanoiTower endState, int step)
-                : base(beginState, goal, move, endState, step)
+            public Simple(THanoiTower beginState, HanoiGoal goal, IndexedHanoiMove move, THanoiTower endState, int step, int depth)
+                : base(beginState, goal, move, endState, step, depth)
             {
             }
         }
@@ -45,8 +47,8 @@
 
             public int EndStep => Step;
 
-            public Complex(THanoiTower beginState, HanoiExplanation<THanoiTower> explanationBeforeMove, HanoiGoal goal, IndexedHanoiMove move, HanoiExplanation<THanoiTower> explanationAfterMove, THanoiTower endState, int step)
-                : base(beginState, goal, move, endState, step)
+            public Complex(THanoiTower beginState, HanoiExplanation<THanoiTower> explanationBeforeMove, HanoiGoal goal, IndexedHanoiMove move, HanoiExplanation<THanoiTower> explanationAfterMove, THanoiTower endState, int step, int depth)
+                : base(beginState, goal, move, endState, step, depth)
             {
                 ExplanationBeforeMove = explanationBeforeMove with { Location = HanoiExplanationLocation.BeforeMove };
                 ExplanationAfterMove = explanationAfterMove with { Location = HanoiExplanationLocation.AfterMove };
@@ -60,12 +62,12 @@
 
     public static class HanoiExplanation
     {
-        public static HanoiExplanation<THanoiTower>.Simple Simple<THanoiTower>(THanoiTower beginState, HanoiGoal goal, IndexedHanoiMove move, THanoiTower endState, int step)
+        public static HanoiExplanation<THanoiTower>.Simple Simple<THanoiTower>(THanoiTower beginState, HanoiGoal goal, IndexedHanoiMove move, THanoiTower endState, int step, int depth)
             where THanoiTower : IHanoiTower<THanoiTower> =>
-            new HanoiExplanation<THanoiTower>.Simple(beginState, goal, move, endState, step);
+            new HanoiExplanation<THanoiTower>.Simple(beginState, goal, move, endState, step, depth);
 
-        public static HanoiExplanation<THanoiTower>.Complex Complex<THanoiTower>(THanoiTower beginState, HanoiExplanation<THanoiTower> explanationBeforeMove, HanoiGoal goal, IndexedHanoiMove move, HanoiExplanation<THanoiTower> explanationAfterMove, THanoiTower endState, int step)
+        public static HanoiExplanation<THanoiTower>.Complex Complex<THanoiTower>(THanoiTower beginState, HanoiExplanation<THanoiTower> explanationBeforeMove, HanoiGoal goal, IndexedHanoiMove move, HanoiExplanation<THanoiTower> explanationAfterMove, THanoiTower endState, int step, int depth)
             where THanoiTower : IHanoiTower<THanoiTower> =>
-            new HanoiExplanation<THanoiTower>.Complex(beginState, explanationBeforeMove, goal, move, explanationAfterMove, endState, step);
+            new HanoiExplanation<THanoiTower>.Complex(beginState, explanationBeforeMove, goal, move, explanationAfterMove, endState, step, depth);
     }
 }
